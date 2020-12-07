82784a6ceb25ac339ce627574665b52f059e22ed
Vladimir Kharchev <necrosovereign@gmail.com>
2020-12-07 18:25:13 +0100

    Distance calculating algorithm

--- library/seq.py
+++ library/seq.py
@@ -1,5 +1,7 @@
+from typing import Tuple, Optional
 from Bio.Align import PairwiseAligner
 import os
+import math
 
 
 with open(os.path.join('data', 'scores.tab')) as scores_file:
@@ -25,3 +27,129 @@ except KeyError as ex:
 
 aligner = PairwiseAligner(match_score=MATCH_SCORE, mismatch_score=MISMATCH_SCORE, end_open_gap_score=END_GAP_PENALTY,
                           end_extend_gap_score=END_GAP_EXTEND_PENALTY, internal_open_gap_score=GAP_PENALTY, internal_extend_gap_score=GAP_EXTEND_PENALTY)
+
+
+class Seq():
+    """
+    Wrapper for sequences
+    """
+
+    def __init__(self, sequence: str) -> None:
+        self.seq = sequence
+
+    def __len__(self) -> int:
+        return len(self.seq)
+
+    def align(self, other: 'Seq') -> 'Alignment':
+        return Alignment(aligner.align(self.seq, other.seq)[0].aligned)
+
+
+class Alignment():
+    """
+    Represents the alignment
+    """
+
+    def __init__(self, alignment: Tuple[Tuple[Tuple[int, int], ...], Tuple[Tuple[int, int], ...]]) -> None:
+        self.alignment = alignment
+
+    @classmethod
+    def already_aligned(cls, seq: Seq) -> 'Alignment':
+        return cls((((0, len(seq)),), ((0, len(seq)),)))
+
+
+Fragment = Tuple[Tuple[int, int], Tuple[int, int]]
+
+
+class AlignmentStats():
+    """
+    Summary of alignment information
+    """
+
+    def __init__(self) -> None:
+        self.total_length = 0
+        self.common_length = 0
+        self.total_gap_length = 0
+        self.transitions = 0
+        self.transversions = 0
+        self._previous_target_end: Optional[int] = None
+        self._previous_query_end: Optional[int] = None
+
+    @property
+    def substitions(self) -> int:
+        return self.transitions + self.transversions
+
+    def calculate(self, alignment: Alignment, target: Seq, query: Seq) -> None:
+        """
+        Calculates the stats, when alignment is the alignment of target and query
+        """
+        (target_fragments, query_fragments) = alignment.alignment
+        for fragment in zip(target_fragments, query_fragments):
+            self.update(fragment, target.seq, query.seq)
+
+    def pdistance(self) -> float:
+        return self.substitions / self.common_length
+
+    def pdistance_counting_gaps(self) -> float:
+        return (self.substitions + self.total_gap_length) / self.common_length
+
+    def jukes_cantor_distance(self) -> float:
+        p = self.substitions / self.common_length
+        return - (3 / 4) * math.log(1 - (4 / 3) * p)
+
+    def kimura2p_distance(self) -> float:
+        p = self.transitions / self.common_length
+        q = self.transversions / self.common_length
+        return - (1 / 2) * math.log((1 - 2 * p - q) * math.sqrt(1 - 2 * q))
+
+    def update(self, frag: Fragment, target: str, query: str) -> None:
+        self.update_total_length(frag)
+        self.update_common_length(frag)
+        self.update_total_gap_length(frag)
+        self.update_transitions_and_transversions(frag, target, query)
+        self._update_previous_target_end(frag)
+        self._update_previous_query_end(frag)
+
+    def update_total_length(self, frag: Fragment) -> None:
+        ((target_start, target_end), (query_start, query_end)) = frag
+        self.total_length += target_end - target_start + \
+            self.target_gap(target_start) + self.query_gap(query_start)
+
+    def update_common_length(self, frag: Fragment) -> None:
+        ((target_start, target_end), _) = frag
+        self.common_length += target_end - target_start
+
+    def update_total_gap_length(self, frag: Fragment) -> None:
+        ((target_start, _), (query_start, _)) = frag
+        self.total_gap_length += self.target_gap(
+            target_start) + self.query_gap(query_start)
+
+    def update_transitions_and_transversions(self, frag: Fragment, target: str, query: str) -> None:
+        (target_frag, query_frag) = frag
+        for pair in zip(target[slice(target_frag)], query[slice(query_frag)]):
+            if pair[0] == pair[0]:
+                continue
+            else:
+                self.transitions += AlignmentStats.is_transition(pair)
+                self.transversions += 1 - AlignmentStats.is_transition(pair)
+
+    def _update_previous_target_end(self, frag: Fragment) -> None:
+        self._previous_target_end = frag[0][1]
+
+    def _update_previous_query_end(self, frag: Fragment) -> None:
+        self._previous_query_end = frag[1][1]
+
+    @staticmethod
+    def is_transition(pair: Tuple[str, str]) -> bool:
+        return pair in {('a', 'g'), ('g', 'a'), ('c', 't'), ('t', 'c')}
+
+    def target_gap(self, target_start: int) -> int:
+        if self._previous_target_end:
+            return target_start - self._previous_target_end
+        else:
+            return 0
+
+    def query_gap(self, query_start: int) -> int:
+        if self._previous_query_end:
+            return query_start - self._previous_query_end
+        else:
+            return 0
