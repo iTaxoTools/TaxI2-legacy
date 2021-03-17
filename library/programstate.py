Put calculation of alignment into correct branch

library/programstate.py
@@ -413,10 +413,10 @@ def make_distance_table(table: pd.DataFrame, already_aligned: bool) -> pd.DataFr
     The entries are arrays of pairwise distances
     """
     if already_aligned:
-        distance_array = seq_distances_ufunc.outer(
+        distance_array = seq_distances_aligned_ufunc.outer(
             np.asarray(table["sequence"]), np.asarray(table["sequence"]))
     else:
-        distance_array = seq_distances_aligned_ufunc.outer(
+        distance_array = seq_distances_ufunc.outer(
             np.asarray(table["sequence"]), np.asarray(table["sequence"]))
     # prepare indices
     seqid1 = table.index.copy()

library/seq.py
@@ -214,4 +214,5 @@ def seq_distance_aligned(target: str, query: str) -> np.array:
 
 
 seq_distances_ufunc: np.ufunc = np.frompyfunc(seq_distances, 2, 1)
-seq_distances_aligned_ufunc: np.ufunc = np.frompyfunc(seq_distances, 2, 1)
+seq_distances_aligned_ufunc: np.ufunc = np.frompyfunc(
+    seq_distance_aligned, 2, 1)